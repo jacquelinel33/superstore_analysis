1c76cd52466e87e363a6e1450cd80e6b64acf4ed
Jacqueline Lee <jlee4332@gmail.com>
2025-10-24 12:07:16 -0700

    create sql table

--- Superstore.sql
+++ Superstore.sql
@@ -1,35 +1,28 @@
-CREATE DATABASE superstore_db;
 
 CREATE TABLE orders (
-    row_id INTEGER PRIMARY KEY,
-    order_id VARCHAR(50),
-    order_date DATE,
-    ship_date DATE,
-    ship_mode VARCHAR(50),
-    customer_id VARCHAR(50),
-    customer_name VARCHAR(100),
-    segment VARCHAR(50),
-    country VARCHAR(100),
-    city VARCHAR(100),
-    state VARCHAR(50),
-    postal_code VARCHAR(20),
-    region VARCHAR(50),
-    product_id VARCHAR(50),
-    category VARCHAR(50),
-    sub_category VARCHAR(50),
-    product_name VARCHAR(255),
-    sales DECIMAL(10,2),
-    quantity INTEGER,
-    discount DECIMAL(5,4),
-    profit DECIMAL(10,2)
+    Row_ID INT PRIMARY KEY,
+    Order_ID VARCHAR(20),
+    Order_Date DATE,
+    Ship_Date DATE,
+    Ship_Mode VARCHAR(50),
+    Customer_ID VARCHAR(20),
+    Customer_Name VARCHAR(100),
+    Segment VARCHAR(50),
+    Country VARCHAR(50),
+    City VARCHAR(100),
+    State VARCHAR(50),
+    Postal_Code VARCHAR(20),
+    Region VARCHAR(50),
+    Product_ID VARCHAR(20),
+    Category VARCHAR(50),
+    Sub_Category VARCHAR(50),
+    Product_Name VARCHAR(255),
+    Sales DECIMAL(10, 2),
+    Quantity INT,
+    Discount DECIMAL(4, 2),
+    Profit DECIMAL(10, 4)
 );
 
-COPY orders(
-    row_id, order_id, order_date, ship_date, ship_mode,
-    customer_id, customer_name, segment, country, city,
-    state, postal_code, region, product_id, category,
-    sub_category, product_name, sales, quantity, discount, profit
-)
-FROM '/Users/jacquelinelee/Desktop/Data-Projects/Superstore Project/Sample - Superstore 2.csv'
-DELIMITER ','
-CSV HEADER;
+copy orders FROM '/Users/jacquelinelee/Desktop/Data-Projects/Superstore Project/Sample - Superstore 2.csv' WITH (FORMAT csv, HEADER true, ENCODING 'WIN1252');
+
+SELECT * FROM orders LIMIT 5
